7e0656add6c2abde9c3b09dd6320ba33d47e3b81
Jigar Parekh <jigarparekh002@gmail.com>
2017-04-06 01:30:28 +0530

    Designed UI to Netflix theme

--- randomApp/MainVC.swift
+++ randomApp/MainVC.swift
@@ -55,16 +55,16 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
-        let p1 = cellModel(imageURL: "https://i.ytimg.com/vi/CD-E-LDc384/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=FkDX6qK754VnSozVHtW8Lw15bHY", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CD-E-LDc384\" frameborder=\"0\" allowfullscreen></iframe>" , videoTitle: "Metallica - Enter Sandman [Official Music Video]")
+        let p1 = cellModel(imageURL: "https://i.ytimg.com/vi/sbMgYMY9Ryw/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=Af7ibynSxhyVi4XmRRnxMLrr8LQ", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/sbMgYMY9Ryw\" frameborder=\"0\" allowfullscreen></iframe>" , videoTitle: "Breaking Bad")
         
         
-        let p2 = cellModel(imageURL: "https://i.ytimg.com/vi/UprcpdwuwCg/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=oLudMeTATxQmMyBOiJzfaVV9Tu0", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/UprcpdwuwCg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "twenty one pilots: Heathens (from Suicide Squad: The Album) [OFFICIAL VIDEO]")
+        let p2 = cellModel(imageURL: "https://i.ytimg.com/vi/doku-BRZpD0/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=67&sigh=7YK_8tYXBKMLZQsozKlUXZe8SpI", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/U7elNhHwgBU\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Narcos")
         
-        let p3 = cellModel(imageURL: "https://i.ytimg.com/vi/60ItHLz5WEA/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=67&sigh=duu4ukZNwPRxjNrVehmMouWL6zI" , videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/60ItHLz5WEA\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "Alan Walker - Faded")
+        let p3 = cellModel(imageURL: "https://i.ytimg.com/vi/JebwYGn5Z3E/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=QSGfUaeGawmpUPITWdqyx7Hd_GI" , videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/JebwYGn5Z3E\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "13 Reasons Why")
         
-        let p4 = cellModel(imageURL: "hhttps://i.ytimg.com/vi/CD-E-LDc384/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=FkDX6qK754VnSozVHtW8Lw15bHY", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/CD-E-LDc384\" frameborder=\"0\" allowfullscreen></iframe>" , videoTitle: "Metallica - Enter Sandman [Official Music Video]")
+        let p4 = cellModel(imageURL: "https://i.ytimg.com/vi/5kRo6Yg091o/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=67&sigh=Jhdi-joUHteFCWU7HmuaWUkM3eE", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/5kRo6Yg091o\" frameborder=\"0\" allowfullscreen></iframe>" , videoTitle: "Stranger Things")
         
-        let p5 = cellModel(imageURL: "https://i.ytimg.com/vi/UprcpdwuwCg/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=oLudMeTATxQmMyBOiJzfaVV9Tu0", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/UprcpdwuwCg\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "twenty one pilots: Heathens (from Suicide Squad: The Album) [OFFICIAL VIDEO]")
+        let p5 = cellModel(imageURL: "https://i.ytimg.com/vi/gBabKoHSErI/hqdefault.jpg?custom=true&w=336&h=188&stc=true&jpg444=true&jpgq=90&sp=68&sigh=_OdxNekwNds-u-yhVITInq3-Qw4", videoURL: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/ULwUzF1q5w4\" frameborder=\"0\" allowfullscreen></iframe>", videoTitle: "House of Cards")
         
         items.append(p1)
         items.append(p2)
